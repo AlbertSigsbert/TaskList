b35550ab34162dbbd212b273f5d685495732bb90
Albert <ishengomaalbert@gmail.com>
2021-04-19 12:26:31 +0300

    Add clearTasks & filterTasks features

--- dist/main.js
+++ dist/main.js
@@ -3,24 +3,33 @@ const taskForm = document.querySelector('form');
 const addTaskBtn = document.getElementById('add-task');
 const taskInput = document.getElementById('task-input');
 const tasks = document.getElementById('tasks');
+const clearTasks = document.getElementById('clear-task');
+const filterInput = document.getElementById('filter-input');
 
 //Load Event listeners
 loadEventListeners();
 
 //The loadEventlisteners fn
 function loadEventListeners(){
+   //Add task Event
    taskForm.addEventListener('submit' , addTask);
+   //Remove task Event
    tasks.addEventListener('click' , removeTask);
+   //Clear tasks Event
+   clearTasks.addEventListener('click', clearAllTasks);
+   //Filter tasks
+   filterInput.addEventListener('keyup', filterTasks);
+
 }
 
-//The eventHandler 
+//The addTask eventHandler 
 function addTask(e){
    
    if(taskInput.value !== '')
    {
        //Creating a task
     const task = document.createElement('li');
-    task.className ='mt-3 flex bg-green-200  px-4 py-2 shadow-md rounded-md hover:bg-green-300';
+    task.className ='task mt-3 flex bg-green-200  px-4 py-2 shadow-md rounded-md hover:bg-green-300';
 
     //Creating a paragraph
     const para = document.createElement('p');
@@ -43,7 +52,7 @@ function addTask(e){
      //clear the input
       taskInput.value = '';
 
-      console.log(tasksUl) 
+      // console.log(tasksUl) 
    }
 
   
@@ -59,4 +68,37 @@ function removeTask(e){
       e.target.parentElement.parentElement.remove();
      }
    }
+}
+
+//Clear tasks eventHandler
+function clearAllTasks(){
+   // tasks.innerHTML = '';
+
+   if(confirm('Do you want to clear all tasks?'))
+   {
+      while(tasks.firstChild)
+      {
+         tasks.removeChild(tasks.firstChild);
+      }
+   
+   }
+  
+}
+
+//Filter tasks eventHandler
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+ // console.log(text);
+
+ document.querySelectorAll('.task').forEach(function(task){
+    const taskName = task.firstChild.textContent.toLowerCase();
+
+    if(taskName.indexOf(text) != -1)
+    {
+       task.classList.add('block');
+    }
+    else{
+      task.classList.add('hidden');
+    }
+ });
 }
